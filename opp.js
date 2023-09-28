540c44ab97e3320fde4e4f17cea52462a78c5a74
mrsithuhein <sithuhein.dev@gmail.com>
2023-09-28 08:00:03 +0630

    js test

--- opp.js
+++ opp.js
@@ -1,20 +1,25 @@
-class Class {
-    constructor(name,age,gender){
-        this.name = name;
-        this.age = age;
-        this.gender = gender;
-        this.namePrefix = gender === "male" ? "U" : "Daw";
-        this.fullName = this.namePrefix + " " + this.name
-        this.bornYear = 2023 - this.age;
-    }
+// class Class {
+//     constructor(name,age,gender){
+//         this.name = name;
+//         this.age = age;
+//         this.gender = gender;
+//         this.namePrefix = gender === "male" ? "U" : "Daw";
+//         this.fullName = this.namePrefix + " " + this.name
+//         this.bornYear = 2023 - this.age;
+//     }
 
-    #private = "hide";
-}
+//     #private = "hide";
+// }
 
-const obj = new Class("sth",15,"female");
-console.log(obj);
-obj.name = "llp";
-console.log(obj); 
-obj.add = 100;
-console.log(obj);
+// const obj = new Class("sth",15,"female");
+// console.log(obj);
+// obj.name = "llp";
+// console.log(obj);
+// obj.add = 100;
+// console.log(obj);
 
+const d = new Date();
+
+console.log(new Date().getFullYear(),new Date().getMonth()+1,new Date().getDate())
+
+console.log(`${d.getDate()}-${d.getMonth()+1}-${d.getFullYear()}`);
