1f2a04d948f4893016cdd5c16a47eb262753709f
mrsithuhein <sithuhein.dev@gmail.com>
2023-10-23 17:04:42 +0630

    random id

--- js-project/2.To-do-lists/js/list.js
+++ js-project/2.To-do-lists/js/list.js
@@ -11,14 +11,30 @@ const countList = () => {
   ).length;
 };
 
+const char = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+const getRanInt = (min, max) => {
+  let result = Math.floor(Math.random() * (max - min)) + min;
+  return result;
+};
+
+const getRanId = (length) => {
+  let result = "";
+  for (let i = 0; i < length; i++) {
+    result += char[getRanInt(0, char.length)];
+  }
+  return result;
+};
+
 const createList = (listText) => {
+  const checkerId = getRanId(10);
   const list = document.createElement("div");
   list.classList.add("list");
   list.innerHTML = `
     <div class="border border-primary p-2 d-flex justify-content-between mb-2">
     <div class="form-check list-checker">
-        <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked">
-        <label class="form-check-label" for="flexCheckChecked">
+        <input class="form-check-input" type="checkbox" value="" id="${checkerId}">
+        <label class="form-check-label" for="${checkerId}">
             ${listText}
         </label>
     </div>
