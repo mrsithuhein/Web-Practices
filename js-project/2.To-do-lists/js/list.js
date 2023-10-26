fffa60351d1bbb4f03e31f03652dfeed2d377316
mrsithuhein <sithuhein.dev@gmail.com>
2023-10-26 17:02:28 +0630

    end

--- js-project/2.To-do-lists/js/list.js
+++ js-project/2.To-do-lists/js/list.js
@@ -1,23 +1,36 @@
+//selectors
 const textInput = document.querySelector("#textInput");
 const addBtn = document.querySelector("#addBtn");
 const doneListCounter = document.querySelector("#doneListCounter");
 const totalListCounter = document.querySelector("#totalListCounter");
 const lists = document.querySelector("#lists");
 
+//function to get total list count and done list count
 const countList = () => {
-  totalListCounter.innerText = lists.querySelectorAll(".list").length;
+  const totalLists = lists.querySelectorAll(".list").length;
+  totalListCounter.innerText = totalLists;
   doneListCounter.innerText = lists.querySelectorAll(
     ".list-checker [type='checkbox']:checked"
   ).length;
-};
 
+  if (totalLists === 0) {
+    lists.innerHTML = `<p class="text-center empty-state">There is no list</p>`;
+  } else {
+    lists.querySelector(".empty-state")?.remove();
+  }
+};
+countList();
+
+//start of generate random id process
 const char = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
+//to get random integer to use as index number in getRanId function
 const getRanInt = (min, max) => {
   let result = Math.floor(Math.random() * (max - min)) + min;
   return result;
 };
 
+//get random id with char by using index number from getRanInt function
 const getRanId = (length) => {
   let result = "";
   for (let i = 0; i < length; i++) {
@@ -25,7 +38,9 @@ const getRanId = (length) => {
   }
   return result;
 };
+//end of Generate random id process
 
+//start of create new list process
 const createList = (listText) => {
   const checkerId = getRanId(10);
   const list = document.createElement("div");
@@ -50,6 +65,7 @@ const createList = (listText) => {
     `;
   textInput.value = null;
 
+  //delete button to delete list
   const deleteBtn = list.querySelector(".deleteBtn");
   deleteBtn.addEventListener("click", () => {
     const confirm = window.confirm("Are you sure to delete?");
@@ -57,6 +73,7 @@ const createList = (listText) => {
     countList();
   });
 
+  //edit button to edit list
   const editBtn = list.querySelector(".editBtn");
   const listLabel = list.querySelector(".list-label");
 
@@ -71,7 +88,9 @@ const createList = (listText) => {
       listLabel.innerText = editInput.value;
     });
   });
+  //end of create new list process
 
+  //when list is checked to call count list function to update done list count
   const listChecker = list.querySelector(".list-checker");
   listChecker.addEventListener("click", () => {
     countList();
@@ -80,6 +99,7 @@ const createList = (listText) => {
   return list;
 };
 
+//add button to create new list
 addBtn.addEventListener("click", () => {
   if (textInput.value != "") {
     lists.append(createList(textInput.value));
